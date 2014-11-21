721e7b1418640ae6b7c3b14e5f9887ea9e780008
Sean Callan <seancallan@gmail.com>
2014-11-20 23:02:00 -0800

    Remove memcache setup

--- opsworks_sidekiq/attributes/default.rb
+++ opsworks_sidekiq/attributes/default.rb
@@ -7,4 +7,3 @@ node[:deploy].each do |application, deploy|
   default[:sidekiq][application.intern][:restart_command] = "sudo monit restart -g sidekiq_#{application}_group"
   default[:sidekiq][application.intern][:syslog] = false
 end
-
--- opsworks_sidekiq/recipes/configure.rb
+++ opsworks_sidekiq/recipes/configure.rb
@@ -23,21 +23,4 @@ node[:deploy].each do |application, deploy|
     end
   end
 
-  template "#{deploy[:deploy_to]}/shared/config/memcached.yml" do
-    source "memcached.yml.erb"
-    cookbook 'rails'
-    mode "0660"
-    group deploy[:group]
-    owner deploy[:user]
-    variables(
-      :memcached => deploy[:memcached] || {},
-      :environment => deploy[:rails_env]
-    )
-
-    notifies :run, resources(:execute => "restart Rails app #{application}")
-
-    only_if do
-      File.exists?(deploy[:deploy_to]) && File.exists?("#{deploy[:deploy_to]}/shared/config/")
-    end
-  end
 end
