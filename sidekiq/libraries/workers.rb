3d267bae71c2263a5830ef33051eaab0258e338d
Sean Callan <seancallan@gmail.com>
2014-12-16 21:33:57 -0800

    Include helpers in Templates

--- sidekiq/libraries/workers.rb
+++ sidekiq/libraries/workers.rb
@@ -1,9 +1,13 @@
 module Sidekiq
   module Helpers
     def configured_workers(config)
-      @configured_workers ||= config.to_hash.reject { |k, v| k.to_s =~ /restart_command|syslog/ }
+      @configured_workers ||= config.each_with_object({}) do |(k, v), memo|
+        next if k.to_s =~ /restart_command|syslog/
+        memo[k] = v
+      end
     end
   end
 end
 
 Chef::Recipe.send(:include, Sidekiq::Helpers)
+Chef::Resource::Template.send(:include, Sidekiq::Helpers)
